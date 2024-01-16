8b204e1d331f8821e4ec6f0c5964cd9b8d6fc03e
vikaspatil10 <vikaspatil1010vp@gmail.com>
2024-01-17 00:07:27 +0530

    primeng installed and started  implementation

--- WeatherApp/src/app/app.module.ts
+++ WeatherApp/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { WeatherpageComponent } from './weatherpage/weatherpage.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    WeatherpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ButtonModule,
+    ReactiveFormsModule,HttpClientModule
   ],
-  providers: [],
+    
+    providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
